d034acc28fcaaa187414090e84938f440cfc38da
Jorge <jorge.toledo.6@hotmail.com>
2022-09-22 16:33:30 -0300

    EF

---
+++ ConsoleAppEntityFramework/Model/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleAppEntityFramework.Model
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Producto> tblProductos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BackEnd2022;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Producto>().HasKey(x => x.Rut);
+        }
+
+
+
+    }
+}
---
+++ ConsoleAppEntityFramework/Model/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppEntityFramework.Model
+{
+    public class Producto
+    {
+        //// Id ProductoId
+        //[Key]
+        //public string Rut { get; set; } // PK
+
+        public int Id { get; set; }  //PK
+        public string Name { get; set; }
+        public int Precio{ get; set; }
+        public int Stock{ get; set; }
+        public string UrlImagen{ get; set; }
+
+    }
+}
